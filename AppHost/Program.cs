f9261d909ec07a4ab04bd8718406dd7bd0c33c30
medvedevden1s <medvedevdenis12@gmail.com>
2025-04-24 16:39:44 +0300

    added .ner core api project

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -2,4 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // add procjects and cloud native backing services here
 
+builder.AddProject<Projects.Catalog>("catalog");
+
+// add procjects and cloud native backing services here
+
 builder.Build().Run();
